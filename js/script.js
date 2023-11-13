1462f88d24719902cfe8ebc2aa010fb0e05aacbc
María Delgado <mariad1996@gmail.com>
2023-11-13 17:47:48 +0100

    basic js finished

--- js/script.js
+++ js/script.js
@@ -1,32 +1,44 @@
 import peliculas from './peliculas.js'
 
 
+// Secciones
+
+const divAccion = document.getElementById('genero-28');
+const divThriller = document.getElementById('genero-53');
+const divAventura = document.getElementById('genero-12');
+
+// Filtro
+
+peliculas.filter((element) => element.genre_ids.find((genre) => {
+    let titulo = document.createElement('p');
+    titulo.textContent = element.title;
+
+    let poster = document.createElement('img');
+    poster.src = `https://image.tmdb.org/t/p/w200${element.poster_path}`;
+    poster.alt = `Poster de la película ${element.title}`
+
+    let divPeli = document.createElement('div')
+    divPeli.appendChild(titulo);
+    titulo.insertAdjacentElement('beforebegin', poster);
+
+    if (genre === 28) {
+        divAccion.appendChild(divPeli);
+    } else if (genre === 53) {
+        divThriller.appendChild(divPeli);
+    } else if (genre === 12) {
+        divAventura.appendChild(divPeli);
+    }
+}))
+
+
+
 // Filtros
+
 const filtroAccion = peliculas.filter((elemento) => elemento.genre_ids.find((genero) => genero === 28));
 console.log(filtroAccion)
 
 const filtroThriller = peliculas.filter((elemento) => elemento.genre_ids.find((genero) => genero === 53));
 console.log(filtroThriller)
 
-const filtroAventura = peliculas.filter((elemento) => elemento.genre_ids.find((genero) => genero === 12))
+const filtroAventura = peliculas.filter((elemento) => elemento.genre_ids.find((genero) => genero === 12));
 console.log(filtroAventura)
-
-
-// Asignación a la sección correcta
-const divAccion = document.getElementById('genero-28');
-const divThriller = document.getElementById('genero-53');
-const divAventura = document.getElementById('genero-12');
-
-
-
-for (let i=0; i<filtroAccion.length; i++) {
-    let titulosAccion = document.createElement('p');
-    titulosAccion.textContent = filtroAccion[i].title;
-    let posterAcc = document.createElement('img');
-    posterAcc.src= `https://image.tmdb.org/t/p/w200${filtroAccion[i].poster_path}`;
-
-    let divPeli= document.createElement('div')
-    divPeli.appendChild(titulosAccion);
-    titulosAccion.insertAdjacentElement('beforebegin', posterAcc)
-    divAccion.appendChild(divPeli)
-}
